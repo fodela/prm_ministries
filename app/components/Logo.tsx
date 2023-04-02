b4aaa856b320caf0309d983988103d3ee1309d09
fodela <particleme30@gmail.com>
2023-04-02 07:57:21 +0000

    🐛 fix: <img> could result in slower LCP

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const Logo = () => {
   return (
     <Link href="/" className="flex items-end">
-      <img
+      <Image
         src="https://prmministries.org/wp-content/uploads/2021/09/Picture1.png"
         alt="PRM Ministeries logo"
         height={45}
         width={45}
       />
+
       <div className="flex flex-col">
         <strong className="text-3xl">PRM Ministries</strong>
         <div className="text-sm">The word of God that changes the world</div>
