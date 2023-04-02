c3aef4dc6f3241f4c67ba06a15bd61b69af932f5
fodela <particleme30@gmail.com>
2023-04-02 07:24:51 +0000

    ✨ feat: adds give page

---
+++ app/give/page.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const GivePage = () => {
+  return (
+    <main className="main">
+      <p>
+        For the word of God is quick, and powerful, and sharper than any two
+        edged sword, piercing even to the dividing asunder of soul and spirit,
+        and of the joints and marrow, and is a discerner of the thoughts and
+        intents of the heart.
+        <strong className="block text-right">Hebrews 4:12</strong>
+      </p>
+      Partner with us in making the word of God free and available to all
+      nations. Send us your donation. Because of your support, no matter the
+      amount, someone will hear about Jesus Christ for the very first time
+      today. Will you give your most generous gift right now?
+      {/* [ ]give contact cards */}
+    </main>
+  );
+};
+
+export default GivePage;
