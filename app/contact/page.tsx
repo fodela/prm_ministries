d8b689fb2f17edd8b3e8619c7378c658f1708d3e
fodela <particleme30@gmail.com>
2023-04-02 07:24:35 +0000

    ✨ feat: adds contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="main ">
+      <p>
+        Spirit Life Network (SLN ), is a fast rising movement whose branches are
+        expected to take roots in almost every continent on this planet.
+      </p>
+      <p>In the interim, locate SLN ministries in Ghana, Volta region Hohoe</p>
+      <p>
+        Your questions, comments, inquiries requests and financial contributions
+        are also important to us. Kindly reach us by the following details:
+      </p>
+      <p>PHONE: +233557499758 / +233207183615</p>
+      <p>
+        MAIL : spiritlife4prm@gmail.com / prophetrichardsonministries@gmail.com
+      </p>
+      <p>
+        Please get in touch. Our ever ready expert support team will gladly
+        address your concerns and set up a meeting with you.
+      </p>
+    </main>
+  );
+};
+
+export default page;
