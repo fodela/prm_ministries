e7e01889758af917583f2daac194c814323f1ed9
fodela <particleme30@gmail.com>
2023-04-02 07:56:43 +0000

    🐛 fix: missing "key"

--- app/components/BlogPost.tsx
+++ app/components/BlogPost.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const BlogPost = ({ post }: { post: Post }) => {
   return (
-    <article className=" relative mt-20 pt-60 ml-20 mr-5 border border-black p-5 ">
+    <article
+      key={post.id}
+      className=" relative mt-20 pt-60 ml-20 mr-5 border border-black p-5 "
+    >
       <figure className="blog_image absolute -top-10 -left-10 right-10"></figure>
       <header className="font-semibold">{post.title}</header>
       <p className="mb-5">{post.body}</p>
