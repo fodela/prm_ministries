96771bd68a736c5722acb844ecdcbae6d80f86ec
fodela <particleme30@gmail.com>
2023-04-02 07:24:18 +0000

    ✨ feat: adds about page

---
+++ app/about/page.tsx
@@ -0,0 +1,32 @@
+export const metadata = {
+  title: "About - PRM Ministries",
+};
+
+const AboutPage = () => {
+  return (
+    <main className="max-w-screen-md text-lg">
+      <article>
+        <p>
+          Prophet Richardson Mensah MINISTRIES (PRMM) is a movement for God
+          presided over and led by Prophet Richardson Mensah, who also is the
+          founder of the SPIRIT LIFE NETWORK(SLN) – a vision laid in His Spirit
+          by Elohim for the perfection of His assignments.
+        </p>
+        <p>
+          As a Prophet, teacher, healing minister, and an online television host
+          , Prophet Richardson daily exudes an insatiable passion to reach the
+          peoples of the world with God’s manifested presence, as he offers
+          God-sourced, Spirit Power- driven, and straight-to-the-point
+          heart-touching prophecies – a divine commission he has diligently
+          focused on fulfilling for more than 10 years.
+        </p>
+        <p>
+          You will be richly blessed following the Prophet of our times; never
+          miss a moment !
+        </p>
+      </article>
+    </main>
+  );
+};
+
+export default AboutPage;
